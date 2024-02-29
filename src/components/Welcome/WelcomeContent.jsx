4d68c5fa5c22ba054a2a41e1307b302cbff020f3
AnnaVlasenko18 <131859714+AnnaVlasenko18@users.noreply.github.com>
2024-02-29 19:05:39 +0200

    task_css
    початкові стилі

--- src/components/Welcome/WelcomeContent.jsx
+++ src/components/Welcome/WelcomeContent.jsx
@@ -1,20 +1,26 @@
-import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import css from '../Welcome/WelcomeContent.module.css';
 
 export const WelcomeContent = () => {
   return (
-    <div>
-      <img src="" alt="" />
-      <div>
-        <svg></svg>
-        <h1>Task Pro</h1>
+    <div className={css.welcomeWrapper}>
+      <div className={css.welcomeContainer}>
+        <img src="" alt="" className={css.welcomeIcon} />
+        <div className={css.welcomeLogo}>
+          <svg className={css.welcomeLogoIcon}></svg>
+          <h1 className={css.welcomeLogoTitle}>Task Pro</h1>
+        </div>
+        <p className={css.welcomeText}>
+          Supercharge your productivity and take control of your tasks with Task
+          Pro - Don't wait, start achieving your goals now!
+        </p>
+        <NavLink to="/auth/register" className={css.welcomeRegisterBtn}>
+          Registration
+        </NavLink>
+        <NavLink to="/auth/login" className={css.welcomeLoginBtn}>
+          Log In
+        </NavLink>
       </div>
-      <p>
-        Supercharge your productivity and take control of your tasks with Task
-        Pro - Don't wait, start achieving your goals now!
-      </p>
-      <NavLink to="/auth/register">Registration</NavLink>
-      <NavLink to="/auth/login">Log In</NavLink>
     </div>
   );
 };
