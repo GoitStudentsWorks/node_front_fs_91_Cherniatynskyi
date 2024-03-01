e0fec928050de469bc56e8fae05ed92b745c0092
AnnaVlasenko18 <131859714+AnnaVlasenko18@users.noreply.github.com>
2024-03-01 02:27:26 +0200

    task_css_2
    1. визначитися з hover
    2. не дороблена svg logo
    3. додати глобальні стилі(root, front)

--- src/components/Welcome/WelcomeContent.jsx
+++ src/components/Welcome/WelcomeContent.jsx
@@ -1,13 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import css from '../Welcome/WelcomeContent.module.css';
+import iconImg from '../../images/welcome/boy@2x.png';
+import sprite from '../../images/sprite.svg';
 
 export const WelcomeContent = () => {
   return (
     <div className={css.welcomeWrapper}>
       <div className={css.welcomeContainer}>
-        <img src="" alt="" className={css.welcomeIcon} />
+        <img src={iconImg} alt="boy at laptop" className={css.welcomeIcon} />
         <div className={css.welcomeLogo}>
-          <svg className={css.welcomeLogoIcon}></svg>
+          <svg className={css.welcomeLogoIcon}>
+            <use href={`${sprite}#icon-logo`} />
+          </svg>
           <h1 className={css.welcomeLogoTitle}>Task Pro</h1>
         </div>
         <p className={css.welcomeText}>
@@ -20,9 +24,19 @@ export const WelcomeContent = () => {
         <NavLink to="/auth/login" className={css.welcomeLoginBtn}>
           Log In
         </NavLink>
+        <NavLink to="/auth/login" className={css.welcomeTestBtn}>
+          Test
+        </NavLink>
+        <NavLink to="/auth/login" className={css.welcomeTest}>
+          Test2
+        </NavLink>
       </div>
     </div>
   );
 };
 
 export default WelcomeContent;
+
+// 1. визначитися з hover
+// 2. не дороблена svg logo
+// 3. додати глобальні стилі(root, front)
