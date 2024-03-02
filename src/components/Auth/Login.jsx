711026e28e839295c381ad9ec6d0ff08d70a0295
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2024-03-02 11:25:47 +0100

    login page component

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,55 @@
+import React from 'react';
+import { Formik } from 'formik';
+import { Form, Field, ErrorMessage } from 'formik';
+import css from './Auth.module.css';
+import { AuthNav } from './AuthNav';
+
+// import { useDispatch } from 'react-redux';
+// import { logIn } from 'redux/auth/operations';
+
 export const Login = () => {
+  // const dispatch = useDispatch();
+
+  //   const handleSubmit = e => {
+  //     e.preventDefault();
+  //     const form = e.currentTarget;
+  //     dispatch(
+  //       logIn({
+  //         email: form.elements.email.value,
+  //         password: form.elements.password.value,
+  //       })
+  //     );
+  //     form.reset();
   return (
-    <>
-      <h1>Login Component</h1>
-    </>
+    <div className={css.authPageWrapper}>
+      <div className={css.loginForm}>
+        <AuthNav />
+        <Formik>
+          {/* <Form onSubmit={handleSubmit} autoComplete="off">*/}
+          <Form autoComplete="off">
+            <ErrorMessage name="email" component="div" />
+            <Field
+              className={css.field}
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+            />
+            <ErrorMessage name="password" component="div" />
+            <Field
+              className={css.field}
+              type="password"
+              name="password"
+              placeholder="Confirm a password"
+            />
+
+            <button className={css.button} type="submit">
+              Log In Now
+            </button>
+          </Form>
+        </Formik>
+      </div>
+    </div>
   );
 };
+
+// *Додати іконку і логіку на видимість пароля
