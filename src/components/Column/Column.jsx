13919d7d8baa9128336a556152071a328598ec06
Mark Cherniatynskyi <markushalot34@gmail.com>
2024-03-03 11:41:09 +0200

    add cards view

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,19 +1,20 @@
-import { Card } from 'components/Card/Card'
 import css from './Column.module.css'
 
-export const Column = ({column}) =>{
+export const Column = ({children, column}) =>{
     return (
     <li className={css.columnWrap}>
-        <div className={css.columnTitleWrap}>
-            <h2 className={css.columnTitle}>Title</h2>
-            <div>
-                <button>edit</button>
-                <button>delete</button>
+        <div>
+            <div className={css.columnTitleWrap}>
+                <h2 className={css.columnTitle}>{column.title}</h2>
+                <div>
+                    <button>edit</button>
+                    <button>delete</button>
+                </div>
             </div>
+            <ul className={css.columnList}>
+                {children}
+            </ul>
         </div>
-        <ul className={css.columnList}>
-            <Card/>
-        </ul>
         <button className={css.columnButton}>Add Card</button>
     </li>)
 }
