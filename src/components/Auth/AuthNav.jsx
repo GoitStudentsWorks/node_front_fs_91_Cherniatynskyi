334e0cb3e04a2b109a2b604590cc7757a90487c5
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2024-03-04 15:59:15 +0100

    Update AuthNav.jsx

--- src/components/Auth/AuthNav.jsx
+++ src/components/Auth/AuthNav.jsx
@@ -5,14 +5,26 @@ import { Login } from './Login';
 
 export const AuthNav = () => {
   const { id } = useParams();
+  // const location = useLocation();
+  // const isActive = path => {
+  //   return location.pathname === path ? 'active' : '';
+  // };
 
   return (
     <div className={css.authForm}>
       <div className={css.authNav}>
-        <NavLink className={css.authLink1} to="/auth/register">
+        <NavLink
+          // className={isActive('/auth/register')}
+          className={css.authLink1}
+          to="/auth/register"
+        >
           Registration
         </NavLink>
-        <NavLink className={css.authLink2} to="/auth/login">
+        <NavLink
+          className={css.authLink2}
+          // className={isActive('/auth/login')}
+          to="/auth/login"
+        >
           Log In
         </NavLink>
         {id === 'register' ? <Register /> : <Login />}
