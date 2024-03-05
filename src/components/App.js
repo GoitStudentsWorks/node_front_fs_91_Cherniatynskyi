a025e410f05fb644401d52a66ff7940d2142716a
endryuman <132812966+endryuman@users.noreply.github.com>
2024-03-05 18:56:16 +0200

    NotFound page

--- src/components/App.js
+++ src/components/App.js
@@ -6,29 +6,32 @@ import { Private } from './Routes/Private';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCurrentUser } from '../redux/auth/authThunks';
+import { NotFound } from '../pages/NotFound/NotFound.jsx';
 
-
-const WelcomePage = lazy (() => import ('../pages/WelcomePage/WelcomePage'))
-const ScreensPages = lazy (()=>import ('../components/ScreensPage/ScreensPage'));
-const HomePage = lazy (() => import ('../pages/HomePage/HomePage'))
-const AuthPage = lazy (()=>import ('../pages/AuthPage/AuthPage'));
+const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
+const ScreensPages = lazy(() =>
+  import('../components/ScreensPage/ScreensPage')
+);
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const AuthPage = lazy(() => import('../pages/AuthPage/AuthPage'));
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCurrentUser())
-  }, [dispatch])
-  
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
+
   return (
     // Робоча версія, підсля написання модулів реєстрації/аутентифікації додам Restricted та Private Routes. Якщо будемо використовувати лоадер також додам до Suspense. Іра
     <Suspense>
       <Routes>
-        <Route path='/' element ={<WelcomePage/>} />
-        <Route path='/auth/:id' element ={<Restricted element={AuthPage}/>} />
-        <Route path='/home' element ={<Private element={HomePage}/>} >
-          <Route path=':boardId' element={<ScreensPages/>}/>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/auth/:id" element={<Restricted element={AuthPage} />} />
+        <Route path="/home" element={<Private element={HomePage} />}>
+          <Route path=":boardId" element={<ScreensPages />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
   );
