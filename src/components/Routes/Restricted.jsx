c5f3affbab1149551d2865592e6750b39e54c3d1
Mark Cherniatynskyi <markushalot34@gmail.com>
2024-03-05 12:22:28 +0200

    fix

--- src/components/Routes/Restricted.jsx
+++ src/components/Routes/Restricted.jsx
@@ -2,5 +2,5 @@ import { useSelector } from "react-redux"
 import { Navigate } from "react-router-dom"
 export const Restricted = ({element: Element}) =>{
   const isAuth = useSelector(state => state.auth.token)
-  return isAuth.length>1 ? <Navigate to='/home'/> : <Element/>
+  return isAuth?.length>1 ? <Navigate to='/home'/> : <Element/>
 }
