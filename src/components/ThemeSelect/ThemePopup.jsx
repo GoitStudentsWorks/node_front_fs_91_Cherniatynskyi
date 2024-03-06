6a1f213bbb35517a447f2be86a5779fe935a6a42
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-03-06 13:41:18 +0200

    ThemeSelect

--- src/components/ThemeSelect/ThemePopup.jsx
+++ src/components/ThemeSelect/ThemePopup.jsx
@@ -1,21 +1,45 @@
-import css from './ThemeSelect.module.css'
-import { useDispatch } from 'react-redux'
-import { updaterUserTheme } from '../../redux/auth/authThunks'
+import css from './ThemeSelect.module.css';
+import { useDispatch } from 'react-redux';
+import { updaterUserTheme } from '../../redux/auth/authThunks';
 
-export const ThemePopup = ({onSelectClose, isOpen}) =>{
-    const dispatch = useDispatch()
+export const ThemePopup = ({ onSelectClose, isOpen }) => {
+  const dispatch = useDispatch();
 
-    const handleChangeTheme = (e) =>{
-        console.log(e.target.id)
-        dispatch(updaterUserTheme({theme: `${e.target.id}`}))
-        onSelectClose()
-    }
+  const handleChangeTheme = e => {
+    console.log(e.target.id);
+    dispatch(updaterUserTheme({ theme: `${e.target.id}` }));
+    onSelectClose();
+  };
 
-    return (
-            <ul  data-popup='menu' className={`${css.menuList} ${!isOpen && css.menuListHidden}`}>
-                <li onClick={handleChangeTheme}  data-popup='el' id='light' className={css.menuItem}>Light</li>
-                <li onClick={handleChangeTheme} data-popup='el' id="dark" className={css.menuItem}>Dark</li>
-                <li onClick={handleChangeTheme} data-popup='el' id='violet' className={css.menuItem}>Violet</li>
-            </ul>
-    )
-}
+  return (
+    <ul
+      data-popup="menu"
+      className={`${css.menuList} ${!isOpen && css.menuListHidden}`}
+    >
+      <li
+        onClick={handleChangeTheme}
+        data-popup="el"
+        id="light"
+        className={css.menuItem}
+      >
+        Light
+      </li>
+      <li
+        onClick={handleChangeTheme}
+        data-popup="el"
+        id="dark"
+        className={css.menuItem}
+      >
+        Dark
+      </li>
+      <li
+        onClick={handleChangeTheme}
+        data-popup="el"
+        id="violet"
+        className={css.menuItem}
+      >
+        Violet
+      </li>
+    </ul>
+  );
+};
