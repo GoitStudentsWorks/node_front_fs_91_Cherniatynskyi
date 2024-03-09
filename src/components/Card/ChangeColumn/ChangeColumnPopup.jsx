cec734394cabc10314779f7d63fa28add7fefb01
Mark Cherniatynskyi <markushalot34@gmail.com>
2024-03-09 23:38:19 +0200

    popup fix

--- src/components/Card/ChangeColumn/ChangeColumnPopup.jsx
+++ src/components/Card/ChangeColumn/ChangeColumnPopup.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 export const ChangeColumnPopup = ({isOpen, handleMove, filterVal}) => {
     const stateColumns = useSelector(state => state.columns.columns)
   return (
-        <ul data-popup="changeMenu" className={`${css.popupMenuList} ${!isOpen && css.popupHidden}`}>
+        <ul style={stateColumns.length < 3 ? {marginTop: "30px"} : {} }  data-popup="changeMenu" className={`${css.popupMenuList} ${!isOpen && css.popupHidden}`}>
             {stateColumns.filter(col => col._id !== filterVal).map(col =>{
                 return(
                     <li onClick={()=>handleMove(col._id)}  key={col._id} className={`${css.popupMenuItem}`}>
