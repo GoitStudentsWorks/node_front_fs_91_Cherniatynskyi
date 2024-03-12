121dd1d5bd73b6c5756a0e3611cb3fcc601db66e
Mark Cherniatynskyi <markushalot34@gmail.com>
2024-03-12 19:30:45 +0200

    upd

--- src/components/ThemeSelect/ThemePopup.jsx
+++ src/components/ThemeSelect/ThemePopup.jsx
@@ -1,34 +1,26 @@
 import css from './ThemeSelect.module.css';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { updaterUserTheme } from '../../redux/auth/authThunks';
-import { motion } from 'framer-motion';
 
 export const ThemePopup = ({ onSelectClose, isOpen }) => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.auth.user);
 
   const handleChangeTheme = e => {
-    const selectedTheme = e.target.id;
-
-    localStorage.setItem('theme', selectedTheme);
-    dispatch(updaterUserTheme({ theme: selectedTheme }));
+    console.log(e.target.id);
+    dispatch(updaterUserTheme({ theme: `${e.target.id}` }));
     onSelectClose();
   };
 
   return (
-    <motion.ul
-    animate={{ scale: 1, y: 0, x: 0 }}
-    initial={{scale: 0, y: "-100%", x: "50%"}}
-    transition={{ ease: "easeIn", duration: .05 }}
-  
+    <ul
       data-popup="menu"
-      className={css.menuList}
+      className={`${css.menuList} ${!isOpen && css.menuListHidden}`}
     >
       <li
         onClick={handleChangeTheme}
         data-popup="el"
         id="dark"
-        className={`${css.menuItem} ${user.theme === 'dark' ? css.activ : ''}`}
+        className={css.menuItem}
       >
         Dark
       </li>
@@ -36,7 +28,7 @@ export const ThemePopup = ({ onSelectClose, isOpen }) => {
         onClick={handleChangeTheme}
         data-popup="el"
         id="light"
-        className={`${css.menuItem} ${user.theme === 'light' ? css.activ : ''}`}
+        className={css.menuItem}
       >
         Light
       </li>
@@ -44,12 +36,10 @@ export const ThemePopup = ({ onSelectClose, isOpen }) => {
         onClick={handleChangeTheme}
         data-popup="el"
         id="violet"
-        className={`${css.menuItem} ${
-          user.theme === 'violet' ? css.activ : ''
-        }`}
+        className={css.menuItem}
       >
         Violet
       </li>
-    </motion.ul>
+    </ul>
   );
 };
