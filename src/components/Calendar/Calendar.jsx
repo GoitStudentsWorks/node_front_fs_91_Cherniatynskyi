63adc427fcea899f9ad94754d5d8d09e2715c214
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2024-03-12 15:06:01 +0200

    add-final

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -4,34 +4,67 @@ import { enGB } from 'date-fns/locale';
 import 'react-datepicker/dist/react-datepicker.css';
 import css from './Calendar.module.css';
 import { forwardRef } from 'react';
+import sprite from '../../images/sprite.svg';
+import { format } from 'date-fns';
 
 const CustomInputWrapper = forwardRef(({ value, onClick }, ref) => (
   <button
     type="button"
-    className={css.inputCalendar}
+    className={`${css.inputCalendar} ${css.buttonWithIcon}`}
     onClick={onClick}
     ref={ref}
   >
-    {value}
+    <span>{value}</span>
+    <svg
+      data-popup="popupBtn"
+      className={css.iconChevronDown}
+      width="18"
+      height="18"
+    >
+      <use data-popup="popupBtn" href={`${sprite}#icon-chevron-down`} />
+    </svg>
   </button>
 ));
 
 export function Calendar({ selectedDate, onDateChange }) {
-  const handleDateChange = date => {
+  const handleDateChange = (date) => {
     onDateChange(date);
   };
 
-  // const today = new Date();
-  // const isToday =
-  //   selectedDate.getDate() === today.getDate() &&
-  //   selectedDate.getMonth() === today.getMonth() &&
-  //   selectedDate.getFullYear() === today.getFullYear();
+  const getDateFormat = (date) => {
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  if (
+    date.getDate() === today.getDate() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear()
+  ) {
+    return "'Today', MMMM dd";
+  } else if (
+    date.getDate() === tomorrow.getDate() &&
+    date.getMonth() === tomorrow.getMonth() &&
+    date.getFullYear() === tomorrow.getFullYear()
+  ) {
+    return "'Tomorrow', MMMM dd";
+  } else {
+    return "EEEE, MMMM dd";
+  }
+};
+
+  const isDateDisabled = (date) => {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    return date <= yesterday;
+  };
 
   return (
     <div>
       <DatePicker
         calendarClassName={css.calendar}
-        dayClassName={() => css.day}
+        dayClassName={(date) =>
+          `${css.day} ${isDateDisabled(date) ? css.disabled : ''}`
+        }
         renderCustomHeader={({
           date,
           decreaseMonth,
@@ -45,26 +78,23 @@ export function Calendar({ selectedDate, onDateChange }) {
               onClick={decreaseMonth}
               disabled={prevMonthButtonDisabled}
             >
-              {'<'}
+              {"<"}
             </button>
             <span>
-              {date.toLocaleDateString(enGB, {
-                month: 'long',
-                year: 'numeric',
-              })}
+              {format(date, 'MMMM yyyy', { locale: enGB })}
             </span>
             <button
               type="button"
               onClick={increaseMonth}
               disabled={nextMonthButtonDisabled}
             >
-              {'>'}
+              {">"}
             </button>
           </div>
         )}
         selected={selectedDate}
         onChange={handleDateChange}
-        dateFormat="MMMM d"
+        dateFormat={getDateFormat(selectedDate)}
         minDate={new Date()}
         locale={enGB}
         weekStartsOn={0}
