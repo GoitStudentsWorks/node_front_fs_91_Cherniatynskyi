39e2723d34ab7108e9be9516b6ba9d0776faeb42
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-03-31 22:48:49 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_front_fs_91_Cherniatynskyi@40213d5b40f7389be4ca3c919bf7b47500b68a9f 🚀

---
+++ static/js/581.bc398227.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktask_pro=self.webpackChunktask_pro||[]).push([[581],{1581:(s,a,c)=>{c.d(a,{A:()=>e});c(5043);const e=c.p+"static/media/sprite.c08330baba807f02c572a9ce079b6d2d.svg"}}]);
+//# sourceMappingURL=581.bc398227.chunk.js.map
